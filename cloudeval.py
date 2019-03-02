c7e56f45d08f50230233ff3a334972aaab024eff
forehand-reed <forehand.reed@gmail.com>
2019-03-02 18:47:21 -0500

    cloud model push

--- cloudeval.py
+++ cloudeval.py
@@ -20,89 +20,88 @@ import random
 tf.logging.set_verbosity(tf.logging.FATAL)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-try:
-    def readLyrics(path):
-        with io.open(path, 'r', encoding='utf8') as f:
-            return f.read().lower()
+
+def readLyrics(path):
+    with io.open(path, 'r', encoding='utf8') as f:
+        return f.read().lower()
 
 
-    def train_model(**args):
-        seqLength = 40
-        lyricsPath = "lyrics_filtered.txt"
+def train_model(**args):
+    seqLength = 40
+    lyricsPath = "lyrics_filtered.txt"
+    
+    #read lyrics and get chars
+    text = readLyrics(lyricsPath)  
+    chars = sorted(list(set(text)))
+    '''
+
+    #Make input sequences
+    #create next_chars array for labeling
+    sequences = []
+    next_chars = []
+    for i in range(0, len(text) - seqLength, seqStep):
+        sequences.append(text[i: i + seqLength])
+        next_chars.append(text[i + seqLength])
+
+    '''
+    char_to_index, indices_char = dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
+    '''
+
+    #vectorise characters and strings
+    X = np.zeros((len(sequences), seqLength, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sequences):
+        for t, char in enumerate(sentence):
+            X[i, t, char_to_index[char]] = 1
+        y[i, char_to_index[next_chars[i]]] = 1
+
+
+    #model struct
+    model = build_model(seqLength, chars)
+    '''
+    #read
+    model = load_model("skiModelTheSlumpGod.h5")
+
         
-        #read lyrics and get chars
-        text = readLyrics(lyricsPath)  
-        chars = sorted(list(set(text)))
-        '''
+    for diversity in [.35]:
+        print()
+        generated = ''
+    #choose random sentence number for seed
+        sentence = "Hey my name is Torin and Im here to say "
+        
+        sentence = sentence.lower()
+        generated += sentence
+        sys.stdout.write(generated)
 
-        #Make input sequences
-        #create next_chars array for labeling
-        sequences = []
-        next_chars = []
-        for i in range(0, len(text) - seqLength, seqStep):
-            sequences.append(text[i: i + seqLength])
-            next_chars.append(text[i + seqLength])
-
-        '''
-        char_to_index, indices_char = dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
-        '''
-
-        #vectorise characters and strings
-        X = np.zeros((len(sequences), seqLength, len(chars)), dtype=np.bool)
-        y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
-        for i, sentence in enumerate(sequences):
+        for i in range(400):
+            x = np.zeros((1, seqLength, len(chars)))
             for t, char in enumerate(sentence):
-                X[i, t, char_to_index[char]] = 1
-            y[i, char_to_index[next_chars[i]]] = 1
+                x[0, t, char_to_index[char]] = 1.
+
+            predictions = model.predict(x, verbose=0)[0]
+
+            temperature=1.0
+            preds = predictions
+            if temperature == 0:
+                temperature = 1
+            preds = np.asarray(preds).astype('float64')
+            preds = np.log(preds) / temperature
+            exp_preds = np.exp(preds)
+            preds = exp_preds / np.sum(exp_preds)
+            probas = np.random.multinomial(1, preds, 1)
+            next_index = np.argmax(probas)
+
+            next_char = indices_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
 
 
-        #model struct
-        model = build_model(seqLength, chars)
-        '''
-        #read
-        model = load_model("skiModelTheSlumpGod.h5")
-
-            
-        for diversity in [.35]:
-            print()
-            generated = ''
-        #choose random sentence number for seed
-            sentence = "Hey my name is Torin and Im here to say "
-            
-            sentence = sentence.lower()
-            generated += sentence
-            sys.stdout.write(generated)
-
-            for i in range(400):
-                x = np.zeros((1, seqLength, len(chars)))
-                for t, char in enumerate(sentence):
-                    x[0, t, char_to_index[char]] = 1.
-
-                predictions = model.predict(x, verbose=0)[0]
-
-                temperature=1.0
-                preds = predictions
-                if temperature == 0:
-                    temperature = 1
-                preds = np.asarray(preds).astype('float64')
-                preds = np.log(preds) / temperature
-                exp_preds = np.exp(preds)
-                preds = exp_preds / np.sum(exp_preds)
-                probas = np.random.multinomial(1, preds, 1)
-                next_index = np.argmax(probas)
-
-                next_char = indices_char[next_index]
-
-                generated += next_char
-                sentence = sentence[1:] + next_char
-
-                sys.stdout.write(next_char)
-                sys.stdout.flush()
-            print()
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     args = parser.parse_args()
--- trainer/model.py
+++ trainer/model.py
@@ -30,53 +30,53 @@ def build_model(sequence_length, chars):
     return model
 
 
-def train_model(train_file='gs://rapgodbucket/halflyrics.txt', job_dir='gs://rapgodbucket/multijobGPU6', **args):
-    with tf.device('/device:GPU:0'):
-        seqLength = 40
-        seqStep = 3
-        epochs = 1
-        diversity = 1.0
-        count = 1
-        characters = file_io.read_file_to_string('gs://rapgodbucket/lyrics_filtered.txt')
-        chars = sorted(list(set(characters)))
-        #model struct
-        model = build_model(seqLength, chars)
-        
+def train_model(train_file='gs://rapgodbucket/halflyrics.txt', job_dir='gs://rapgodbucket/jobFinal', **args):
 
-        #read lyrics and get chars
-        text = file_io.read_file_to_string('gs://rapgodbucket/lyrics_filtered.txt')
-        #gcs_file = gcs.open(train_file)
-        #text = gcs_file.read()  
-        
+    seqLength = 40
+    seqStep = 3
+    epochs = 1
+    diversity = 1.0
+    count = 1
+    characters = file_io.read_file_to_string('gs://rapgodbucket/lyrics_filtered.txt')
+    chars = sorted(list(set(characters)))
+    #model struct
+    model = build_model(seqLength, chars)
+    
 
-
-        #Make input sequences
-        #create next_chars array for labeling
-        sequences = []
-        next_chars = []
-        for i in range(0, len(text) - seqLength, seqStep):
-            sequences.append(text[i: i + seqLength])
-            next_chars.append(text[i + seqLength])
+    #read lyrics and get chars
+    text = file_io.read_file_to_string('gs://rapgodbucket/lyrics_filtered.txt')
+    #gcs_file = gcs.open(train_file)
+    #text = gcs_file.read()  
+    
 
 
-        char_to_index, indices_char = dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
+    #Make input sequences
+    #create next_chars array for labeling
+    sequences = []
+    next_chars = []
+    for i in range(0, len(text) - seqLength, seqStep):
+        sequences.append(text[i: i + seqLength])
+        next_chars.append(text[i + seqLength])
 
 
-        #vectorise characters and strings
-        X = np.zeros((len(sequences), seqLength, len(chars)), dtype=np.bool)
-        y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
-        for i, sentence in enumerate(sequences):
-            for t, char in enumerate(sentence):
-                X[i, t, char_to_index[char]] = 1
-            y[i, char_to_index[next_chars[i]]] = 1
+    char_to_index, indices_char = dict((c, i) for i, c in enumerate(chars)), dict((i, c) for i, c in enumerate(chars))
 
-        #train
-        model.fit(X, y, batch_size=256, nb_epoch=epochs)
 
-        model.save('model.h5')
-        with file_io.FileIO('model.h5', mode='r') as input_f:
-            with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
-                output_f.write(input_f.read())
+    #vectorise characters and strings
+    X = np.zeros((len(sequences), seqLength, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
+    for i, sentence in enumerate(sequences):
+        for t, char in enumerate(sentence):
+            X[i, t, char_to_index[char]] = 1
+        y[i, char_to_index[next_chars[i]]] = 1
+
+    #train
+    model.fit(X, y, batch_size=256, nb_epoch=epochs)
+
+    model.save('model.h5')
+    with file_io.FileIO('model.h5', mode='r') as input_f:
+        with file_io.FileIO(job_dir + '/model.h5', mode='w+') as output_f:
+            output_f.write(input_f.read())
 
 
 if __name__ == '__main__':
